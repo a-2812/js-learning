c495d5c06a6c35b8614a12cc89b4cffe31f69a5c
a-2812 <abdullahahmedtanveer@gmail.com>
2025-08-06 11:19:08 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accId = 144553  //cant be changed
+let accEmail = "abdllah@example.com"
+var accPass = "12345"
+accCity = "Rwp"
+let accState
+
+// accId = 2 // not allowed
+
+
+accEmail = "jsjsj"
+
+accPass = "apple"
+
+accCity = "like"
+
+/*
+prefer not to use 'var'
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accId, accEmail, accPass, accCity,accState])
