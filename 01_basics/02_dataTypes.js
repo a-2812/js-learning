176e9731a177215ccb4b05c339aa419b1f914107
a-2812 <abdullahahmedtanveer@gmail.com>
2025-08-06 11:40:25 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,3 +2,30 @@
 
 // alert(3 + 3) // we are using node js not browser
 
+
+let name = "abdullah" //string datatype
+let age = 12 //number datatype
+let isLoggedIn = true //boolean datatype
+
+
+// number => 2 to power of 53
+// bigint
+// string => ""
+// boolean => true/false
+
+// null => standalone value, representation of empty value
+let country = null
+// undefined => when value is not defined, declared value but not defined value
+// symbol => unique
+
+// object 
+
+
+
+
+console.log(typeof null)  // object
+console.log(typeof undefined) // undefined
+
+
+console.log(typeof "abdullah");
+console.table([typeof name, typeof age, typeof isLoggedIn, typeof null, typeof undefined ])
