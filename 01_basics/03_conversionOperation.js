734618a020a0834a84c83e3094bef4923632d85d
a-2812 <abdullahahmedtanveer@gmail.com>
2025-08-06 12:05:59 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,37 @@
+let score = "apple"
+
+// console.log(typeof score);
+// or
+// console.log(typeof(score));
+
+
+let valueInNumber = Number(score);
+
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber)
+
+
+// "33" => 33
+
+// "33abc" => NaN
+
+// true => 1; false => 0
+
+
+let isLoggedIn = "apple";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "apple" => true
+
+
+let someNumber = 33;  //type => number
+
+let stringSomeNumber = String(someNumber);
+
+console.log(stringSomeNumber)
+console.log(typeof stringSomeNumber)
