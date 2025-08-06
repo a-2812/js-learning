c0b84dd4fc1ecc9b7e50079565b43c03b8749c0c
a-2812 <abdullahahmedtanveer@gmail.com>
2025-08-06 13:10:27 +0000

    operations on datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -33,5 +33,48 @@ let someNumber = 33;  //type => number
 
 let stringSomeNumber = String(someNumber);
 
-console.log(stringSomeNumber)
-console.log(typeof stringSomeNumber)
+// console.log(stringSomeNumber)
+// console.log(typeof stringSomeNumber)
+
+
+
+
+// ******************* Operrations ***********************//
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);  // -3
+
+
+// console.log(3+3)
+// console.log(3-3)
+// console.log(3*3)
+// console.log(3/3)
+// console.log(3**3)
+// console.log(3%2)
+
+
+let str1 = "Hello"
+let str2 = " World"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "3")
+// console.log("1" + "2")
+
+
+let counter = 100
+
+counter++
+// ++counter
+
+// console.log(counter);
+
+
+// console.log(counter++); // 100
+// console.log(++counter);  // 101
+
+
+
