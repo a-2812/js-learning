367d05a567ebed81a371d3a1caaf2357937f3e88
a-2812 <abdullahahmedtanveer@gmail.com>
2025-08-07 09:14:53 +0000

    comparison operators

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 != 1);
+// console.log(2 == 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// console.log(null > 0);   
+// console.log(null >= 0);
+// console.log(null == 0);  // these comparisons are not strict they also allow conversion
+
+
+// console.log(undefined == 0);
+// console.log(undefined == 0);
+// console.log(undefined == 0);
+
+
+// ===  strictly check value also datatype
+
+console.log("2" === 2);  // false
